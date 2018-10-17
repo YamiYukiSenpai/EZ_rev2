a3b47118ab7b019ee03d1f94c8bc1e88eb4b9dff
rfmaynard <42976498+rfmaynard@users.noreply.github.com>
2018-10-17 19:38:45 -0400

    initial

---
+++ eztracker1/app/src/main/java/com/example/ryan/myapplication/RegistrationActivity.java
@@ -0,0 +1,38 @@
+package com.example.ryan.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        Button registerAccount = findViewById(R.id.registerRegister);
+        Button registerBack = findViewById(R.id.registerBackButton);
+
+        registerAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        registerBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
