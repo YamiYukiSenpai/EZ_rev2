33b16db1874971c1bbb0a294915a765eda1146e6
rfmaynard <42976498+rfmaynard@users.noreply.github.com>
2018-10-18 00:34:44 -0400

    work

--- eztracker1/app/src/main/java/com/example/ryan/myapplication/HomeActivity.java
+++ eztracker1/app/src/main/java/com/example/ryan/myapplication/HomeActivity.java
@@ -2,6 +2,12 @@ package com.example.ryan.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final Button settingsBtn = findViewById(R.id.homeSettings);
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(HomeActivity.this, settingsBtn);
+
+                popup.getMenuInflater().inflate(R.menu.home_menu, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch(item.getItemId()){
+                            case R.id.menuQuit:
+                                finishAndRemoveTask();
+                                return true;
+                        }
+                        return HomeActivity.super.onOptionsItemSelected(item);
+                    }
+
+                });
+                popup.show();
+            }
+
+        });
+
     }
 }
