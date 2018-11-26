8ef951caacd5b42ca2fa72696e5e906d2c7c1d20
Jonas Gamao <jonty.68@hotmail.com>
2018-11-26 14:08:41 -0500

    Updated Reset Password activity

--- eztracker1/app/src/main/java/com/example/ryan/myapplication/ResetPassword.java
+++ eztracker1/app/src/main/java/com/example/ryan/myapplication/ResetPassword.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,11 +11,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class ResetPassword extends AppCompatActivity {
 
@@ -36,10 +31,10 @@ public class ResetPassword extends AppCompatActivity {
 
         verify_email.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String email = reset_email.getText().toString().trim();
-               /* if (TextUtils.isEmpty(email)) {
+                final String email = reset_email.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
                     Toast.makeText(ResetPassword.this, "E-mail field empty", Toast.LENGTH_SHORT).show();
-                }*/
+                }
 
                 firebase_auth.sendPasswordResetEmail(email)
                         .addOnCompleteListener(new OnCompleteListener<Void>() {
